3396e1e9e2de8c9d15f34895307aa6601c1fd874
Drew Baker <drewrb@gmail.com>
2024-08-02 23:29:40 -0700

    Better template examples

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php use_component('header'); ?>
+
+<main class="fallback">
+
+    The router.php file had no matching route. This is the fallback template.
+
+</main>
+
+<?php use_component('footer'); ?>
---
+++ templates/reel.php
@@ -0,0 +1,9 @@
+<?php use_component('header'); ?>
+
+<main class="reel">
+
+    Reel template example
+
+</main>
+
+<?php use_component('footer'); ?>
