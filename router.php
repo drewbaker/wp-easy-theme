fd2dc0e01477414a8fd9f49740de8ce7c6117afc
Drew Baker <drewrb@gmail.com>
2024-09-19 00:10:47 -0700

    Added some documentation

--- router.php
+++ router.php
@@ -3,10 +3,11 @@
  * Define the templates to use, based on the valid WordPress routes.
  * 
  * Syntax is similar to Express paths in Node
- * The key is the route name, and the value is an array of [path, template]
- * Values can also be strings, in which case the path is the string and the template is the key.
- * If no template set, the key is used as the template name.
- * If
+ * The key is the route name, and the value is an array of [path, layout, template]
+ * Values can also be strings, in which case the path is the string and the template name is the key.
+ *
+ * If no `template` set, the key is used as the template name.
+ * If no `layout` set, then the `/layouts/default.php` file is used.
  * 
  * SEE https://github.com/drewbaker/wp-easy/blob/main/README.md
  */
